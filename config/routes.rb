1db5a9f44c8e582fb354e246cdeffd29fb8a60ee
tqviet05 <tt.viet.tang@tomosia.com>
2022-06-01 17:17:28 +0700

    DOING setup gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
-  get 'categories/show'
-  get 'categories/index'
-  get 'products/show'
-  get 'products/index'
+  devise_for :users
+  as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+  end
+
+  # get 'categories/show'
+  # get 'categories/index'
+  # get 'products/show'
+  # get 'products/index'
   # get 'home/index'
   # get 'home/details'
   # get 'home/login'
   resources :home#, only: [:index, :create]
-  resources :products
+  resources :products, only: [:index, :show]
   resources :categories, only: :show
+  resources :users
   root 'home#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
